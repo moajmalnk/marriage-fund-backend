17d708c18e074612fee620325687c76c0e50b7b1
irshad <irshadmglm@gmail.com>
2025-11-30 17:28:41 +0530

    profile upload fixed

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # <--- Import this
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.views.auth import CustomTokenObtainPairView
@@ -18,8 +19,7 @@ urlpatterns = [
     
     path('api/dashboard/stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
     path('api/teams/', TeamStructureView.as_view(), name='team-structure'),
+    
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-
-# Always serve media files (even in production for simplicity in this project)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
